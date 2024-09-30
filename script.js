31716bc75a5f98e608e0be972582eddeed2cabd1
gbaresis <105802898+gbaresis@users.noreply.github.com>
2024-09-30 21:59:48 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('eventForm').addEventListener('submit', function(event) {
     event.preventDefault(); // Prevent form submission
     const eventId = document.getElementById('eventId').value;
-    document.getElementById('output').textContent = `Event ID: ${eventId}`;
+    window.open('http://bo-tradertools.prd-novibet.systems/browser/event-history/' + eventId  + 'settlement', 'blank');
 });
 
